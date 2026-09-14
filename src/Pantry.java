faf8d001e1114f47327118bc029be56abf3f1c94
karolina <k.guzewska@gmail.com>
2026-09-14 12:16:35 +0200

    add ProductStorages methods

--- src/Pantry.java
+++ src/Pantry.java
@@ -15,7 +15,7 @@ public class Pantry {
         storageLocations.add(new StorageLocation("Regał - Półka 2"));
         storageLocations.add(new StorageLocation("Kuchnia - Szuflada A4"));
         //categories = new ArrayList<>();
-        //productStorages = new ArrayList<>();
+        productStorages = new ArrayList<>();
     }
 
     boolean isProductExists(String productName) {
@@ -34,6 +34,14 @@ public class Pantry {
         }
     }
 
+    void addProductStorage (Product product, StorageLocation storageLocation) {
+        productStorages.add(new ProductStorage(product, storageLocation, true));
+    }
+
+    StorageLocation getStorageLocation (int storageLocation) {
+        return storageLocations.get(storageLocation-1);
+    }
+
     int getProductCount() {
         return products.size();
     }
@@ -42,6 +50,14 @@ public class Pantry {
         System.out.println("Produkty w Twojej spiżarni");
         for (Product product : products) {
             System.out.println("Produkt " + product.name + " Kategoria " + product.category);
+        }
+    }
+
+    void showProductsWithLocations() {
+        System.out.println("Produkty w Twojej spiżarni i ich lokalizacja:");
+        for (ProductStorage productStorage : productStorages) {
+            System.out.println("Produkt " + productStorage.product.name + " Kategoria " +productStorage.product.category
+                                + " Lokalizacja " + productStorage.storageLocation);
         }
     }
 
@@ -58,16 +74,20 @@ public class Pantry {
         String productName;
         String categoryName;
         Product product;
+        int storageLocation;
 
     do {
         System.out.println("Dodaj nowy produkt");
         productName = scanner.nextLine();
         System.out.println("Podaj kategorie produktu");
         categoryName = scanner.nextLine();
-
         product = new Product(productName, new Category(categoryName));
-
         pantry.addProduct(product);
+
+        pantry.showStorageLocations();
+        storageLocation = Integer.parseInt(scanner.nextLine());
+        pantry.addProductStorage(product, pantry.getStorageLocation(storageLocation));
+
         System.out.println(""" 
                 Wybierz 1 lub 2:
                 1 - Dodaj kolejny produkt
@@ -77,6 +97,6 @@ public class Pantry {
 
         System.out.println("Ilość produktów w Twojej spiżarni to " + pantry.getProductCount());
         pantry.showProducts();
-        pantry.showStorageLocations();
+        pantry.showProductsWithLocations();
         }
 }
